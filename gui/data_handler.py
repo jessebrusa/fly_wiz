119dd39c18698b6a3dcc79ce4a6b3b8cd46a9ba2
Jesse Brusa <jbrusa@southgaterc.com>
2025-01-15 15:06:30 -0500

    data handler built handles changes

--- gui/data_handler.py
+++ gui/data_handler.py
@@ -1,30 +1,44 @@
-# FILE: gui/data_handler.py
-
 class DataHandler:
     """
-    A class to handle data from the GUI.
+    A class to handle data from the GUI and notify observers of changes.
 
     Attributes
     ----------
     data : dict
         A dictionary to store the data from the GUI.
+    observers : list
+        A list of observer functions to notify when data changes.
 
     Methods
     -------
     update_data(key, value):
-        Updates the data dictionary with the given key-value pair.
+        Updates the data dictionary with the given key-value pair and notifies observers.
     get_data():
         Returns the data dictionary.
+    add_observer(observer):
+        Adds an observer function to the list of observers.
+    remove_observer(observer):
+        Removes an observer function from the list of observers.
+    notify_observers(key, value):
+        Notifies all observer functions of data changes.
     """
     def __init__(self):
         """
-        Initializes the DataHandler with an empty data dictionary.
+        Initializes the DataHandler with an empty data dictionary and an empty list of observers.
         """
-        self.data = {}
+        self.data = {
+            'title': '',
+            'styled_info': '',
+            'text_info': '',
+            'footer': '',
+            'image': '',
+            'flyer': '',
+        }
+        self.observers = []
 
     def update_data(self, key, value):
         """
-        Updates the data dictionary with the given key-value pair.
+        Updates the data dictionary with the given key-value pair and notifies observers.
 
         Parameters
         ----------
@@ -34,6 +48,7 @@ class DataHandler:
             The value for the data.
         """
         self.data[key] = value
+        self.notify_observers(key, value)
 
     def get_data(self):
         """
@@ -45,3 +60,39 @@ class DataHandler:
             The data dictionary.
         """
         return self.data
+
+    def add_observer(self, observer):
+        """
+        Adds an observer function to the list of observers.
+
+        Parameters
+        ----------
+        observer : function
+            The observer function to add.
+        """
+        self.observers.append(observer)
+
+    def remove_observer(self, observer):
+        """
+        Removes an observer function from the list of observers.
+
+        Parameters
+        ----------
+        observer : function
+            The observer function to remove.
+        """
+        self.observers.remove(observer)
+
+    def notify_observers(self, key, value):
+        """
+        Notifies all observer functions of data changes.
+
+        Parameters
+        ----------
+        key : str
+            The key for the data that was changed.
+        value : any
+            The new value for the data.
+        """
+        for observer in self.observers:
+            observer(key, value)
