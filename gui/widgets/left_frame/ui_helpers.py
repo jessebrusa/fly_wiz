7bc293ec70e58b50f6c38adb98827b13958a59be
Jesse Brusa <jbrusa@southgaterc.com>
2025-01-20 18:59:10 -0500

    background color row is first in its section

--- gui/widgets/left_frame/ui_helpers.py
+++ gui/widgets/left_frame/ui_helpers.py
@@ -26,6 +26,21 @@ def create_label_and_buttons(parent, image_handler, open_color_wheel_window, ope
     for widget in parent.winfo_children():
         widget.destroy()
 
+    # Label for background color
+    label_text_3 = "BG Color:"
+    background_label = ttk.Label(parent, text=label_text_3, font=LABEL_FONT, width=10, anchor="center")
+    background_label.grid(row=0, column=0, padx=5, pady=5, sticky="ns")
+
+    # Frame for the background color buttons
+    button_frame_3 = ttk.Frame(parent)
+    button_frame_3.grid(row=0, column=1, columnspan=3, padx=5, pady=2, sticky="nsew")
+
+    color_wheel_button = ttk.Button(button_frame_3, text="Color Wheel", style="Small.TButton", width=12, command=open_color_wheel_window)
+    color_wheel_button.pack(side="left", padx=5)
+
+    gradient_button = ttk.Button(button_frame_3, text="Gradient", style="Small.TButton", width=12, command=lambda: open_color_wheel_window(gradient=True))
+    gradient_button.pack(side="left", padx=5)
+
     # Check if images are selected
     image1_selected = image_handler.data_handler.get_data().get('image1') is not None
     image2_selected = image_handler.data_handler.get_data().get('image2') is not None
@@ -33,11 +48,11 @@ def create_label_and_buttons(parent, image_handler, open_color_wheel_window, ope
     # Label for first image
     label_text_1 = "Image 1" + (" ✓:" if image1_selected else " :")
     label = ttk.Label(parent, text=label_text_1, font=LABEL_FONT, width=10, anchor="center")
-    label.grid(row=0, column=0, padx=5, pady=5, sticky="ns")
+    label.grid(row=1, column=0, padx=5, pady=5, sticky="ns")
 
     # Frame for the first set of buttons
     button_frame_1 = ttk.Frame(parent)
-    button_frame_1.grid(row=0, column=1, columnspan=3, padx=5, pady=2, sticky="nsew")
+    button_frame_1.grid(row=1, column=1, columnspan=3, padx=5, pady=2, sticky="nsew")
 
     browse_button_1 = ttk.Button(button_frame_1, text="Browse", style="Small.TButton", width=8, 
                                  command=lambda: image_handler.browse_image('image1'))
@@ -54,11 +69,11 @@ def create_label_and_buttons(parent, image_handler, open_color_wheel_window, ope
     # Label for second image
     label_text_2 = "Image 2" + (" ✓:" if image2_selected else " :")
     second_image_label = ttk.Label(parent, text=label_text_2, font=LABEL_FONT, width=10, anchor="center")
-    second_image_label.grid(row=1, column=0, padx=5, pady=5, sticky="ns")
+    second_image_label.grid(row=2, column=0, padx=5, pady=5, sticky="ns")
 
     # Frame for the second set of buttons
     button_frame_2 = ttk.Frame(parent)
-    button_frame_2.grid(row=1, column=1, columnspan=3, padx=5, pady=2, sticky="nsew")
+    button_frame_2.grid(row=2, column=1, columnspan=3, padx=5, pady=2, sticky="nsew")
 
     browse_button_2 = ttk.Button(button_frame_2, text="Browse", style="Small.TButton", width=8, 
                                  command=lambda: image_handler.browse_image('image2'))
@@ -71,21 +86,6 @@ def create_label_and_buttons(parent, image_handler, open_color_wheel_window, ope
     remove_button_2 = ttk.Button(button_frame_2, text="Remove", style="Small.TButton", width=8, 
                                  command=lambda: image_handler.remove_image('image2'))
     remove_button_2.pack(side="left", padx=5)
-
-    # Label for background color
-    label_text_3 = "Background:"
-    background_label = ttk.Label(parent, text=label_text_3, font=LABEL_FONT, width=10, anchor="center")
-    background_label.grid(row=2, column=0, padx=5, pady=5, sticky="ns")
-
-    # Frame for the background color buttons
-    button_frame_3 = ttk.Frame(parent)
-    button_frame_3.grid(row=2, column=1, columnspan=3, padx=5, pady=2, sticky="nsew")
-
-    color_wheel_button = ttk.Button(button_frame_3, text="Color Wheel", style="Small.TButton", width=12, command=open_color_wheel_window)
-    color_wheel_button.pack(side="left", padx=5)
-
-    gradient_button = ttk.Button(button_frame_3, text="Gradient", style="Small.TButton", width=12, command=lambda: open_color_wheel_window(gradient=True))
-    gradient_button.pack(side="left", padx=5)
 
     parent.grid_columnconfigure(1, weight=1)
     parent.grid_columnconfigure(2, weight=1)
