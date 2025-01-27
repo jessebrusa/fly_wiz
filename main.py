4c046dc8699b866ac7147b6f8c67c5a46eda6115
Jesse Brusa <jbrusa@southgaterc.com>
2025-01-27 16:36:06 -0500

    main file uses a class rather than a function

--- main.py
+++ main.py
@@ -2,24 +2,29 @@ from data_handler import DataHandler
 from flyer_manipulator import FlyerManipulator
 from gui.main_window import FlyWizGui
 
-def main():
-    """
-    The main function to run the Fly Wiz application.
+class FlyWizApp:
+    def __init__(self):
+        """
+        Initializes the FlyWizApp class.
+        """
+        self.data_handler = DataHandler()
+        self.flyer_manipulator = FlyerManipulator(self.data_handler)
+        self.app = FlyWizGui(self.data_handler, self.flyer_manipulator)
 
-    Initializes the FlyWizGui application and starts the main event loop.
-    """
-    try:
-        data_handler = DataHandler()
-        flyer_manipulator = FlyerManipulator(data_handler)
-        app = FlyWizGui(data_handler, flyer_manipulator)
-        app.mainloop()
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    def run(self):
+        """
+        Runs the FlyWizGui application.
+        """
+        try:
+            self.app.mainloop()
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     """
     Entry point for the script.
 
-    Calls the main function to start the Fly Wiz application.
+    Creates an instance of FlyWizApp and runs the application.
     """
-    main()
+    fly_wiz_app = FlyWizApp()
+    fly_wiz_app.run()
